56be359264139f3d63f837e2f3ee3e1e6e43babf
cristian16b <cristian.budzicz>
2019-12-04 17:03:39 -0300

    creado los tres componentes principales: obra social, laboratorio y paciente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,17 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { ObraSocialComponent } from './obra-social/obra-social.component';
+import { PacienteComponent } from './paciente/paciente.component';
+import { LaboratorioComponent } from './laboratorio/laboratorio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BarraLateralComponent
+    BarraLateralComponent,
+    ObraSocialComponent,
+    PacienteComponent,
+    LaboratorioComponent
   ],
   imports: [
     BrowserModule,
