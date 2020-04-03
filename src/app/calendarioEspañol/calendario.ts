edde4e68d02fd540f40db2ae8e8a12100eb34911
cristian16b <cristian.budzicz@gmail.com>
2020-04-03 12:11:07 -0300

    Resuelto visualizacion en español del calendario

--- src/app/calendarioEspañol/calendario.ts
+++ src/app/calendarioEspañol/calendario.ts
@@ -2,6 +2,7 @@ import { DateAdapter, MatDateFormats } from '@angular/material';
 import { isMoment, Moment } from 'moment';
 import * as moment from 'moment';
 
+// https://github.com/angular/components/issues/7143
 const MONTHS = {
     'long': [
       'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',
@@ -34,33 +35,63 @@ for (let date = 1; date <= 31; date++) {
 }
 
 export class MomentDateAdapter extends DateAdapter<Moment> {
-
+  
+    toIso8601(date: Moment): string {
+      throw new Error("Method not implemented.");
+  }
   invalid(): Moment {
       throw new Error("Method not implemented.");
   }
 
-  private localeData = moment.localeData();
-
+      /**
+   * Gets the year component of the given date.
+   * @param date The date to extract the year from.
+   * @returns The year component.
+   */
   getYear(date: Moment): number {
     return date.year();
   }
 
+  /**
+   * Gets the month component of the given date.
+   * @param date The date to extract the month from.
+   * @returns The month component (0-indexed, 0 = January).
+   */
   getMonth(date: Moment): number {
     return date.month();
   }
 
+  /**
+   * Gets the date of the month component of the given date.
+   * @param date The date to extract the date of the month from.
+   * @returns The month component (1-indexed, 1 = first of month).
+   */
   getDate(date: Moment): number {
     return date.date();
   }
 
+  /**
+   * Gets the day of the week component of the given date.
+   * @param date The date to extract the day of the week from.
+   * @returns The month component (0-indexed, 0 = Sunday).
+   */
   getDayOfWeek(date: Moment): number {
     return date.day();
   }
 
+  /**
+   * Gets a list of names for the months.
+   * @param style The naming style (e.g. long = 'January', short = 'Jan', narrow = 'J').
+   * @returns An ordered list of all month names, starting with January.
+   */
   getMonthNames(style: 'long' | 'short' | 'narrow'): string[] {
     return MONTHS[style];
   }
 
+  /**
+   * Gets a list of names for the dates of the month.
+   * @returns An ordered list of all date of the month names, starting with '1'.
+   */
   getDateNames(): string[] {
     const dateNames = [];
     for (let i = 1; i <= 31; i++) {
@@ -70,104 +101,155 @@ export class MomentDateAdapter extends DateAdapter<Moment> {
     return dateNames;
   }
 
+  /**
+   * Gets a list of names for the days of the week.
+   * @param style The naming style (e.g. long = 'Sunday', short = 'Sun', narrow = 'S').
+   * @returns An ordered list of all weekday names, starting with Sunday.
+   */
   getDayOfWeekNames(style: 'long' | 'short' | 'narrow'): string[] {
     return DAYS[style];
   }
 
+  /**
+   * Gets the name for the year of the given date.
+   * @param date The date to get the year name for.
+   * @returns The name of the given year (e.g. '2017').
+   */
   getYearName(date: Moment): string {
-    return String(date.year());
+    return date.year().toString();
   }
 
+  /**
+   * Gets the first day of the week.
+   * @returns The first day of the week (0-indexed, 0 = Sunday).
+   */
   getFirstDayOfWeek(): number {
     return 1; // Monday
   }
 
+  /**
+   * Gets the number of days in the month of the given date.
+   * @param date The date whose month should be checked.
+   * @returns The number of days in the month of the given date.
+   */
   getNumDaysInMonth(date: Moment): number {
     return date.daysInMonth();
   }
 
+  /**
+   * Clones the given date.
+   * @param date The date to clone
+   * @returns A new date equal to the given date.
+   */
   clone(date: Moment): Moment {
     return date.clone();
   }
 
+  /**
+   * Creates a date with the given year, month, and date. Does not allow over/under-flow of the
+   * month and date.
+   * @param year The full year of the date. (e.g. 89 means the year 89, not the year 1989).
+   * @param month The month of the date (0-indexed, 0 = January). Must be an integer 0 - 11.
+   * @param date The date of month of the date. Must be an integer 1 - length of the given month.
+   * @returns The new date, or null if invalid.
+   */
   createDate(year: number, month: number, date: number): Moment {
-    return moment([year, month, date]);
+    const value = year + '-' + (month + 1) + '-' + date;
+    const theDate = moment(value, 'YYYY-MM-DD');
+    return theDate.isValid() ? theDate : null;
   }
 
+  /**
+   * Gets today's date.
+   * @returns Today's date.
+   */
   today(): Moment {
     return moment();
   }
 
-  parse(value: any, parseFormat: any): Moment {
-    let m = moment(value, parseFormat, true);
-    if (!m.isValid()) {
-      m = moment(value);
-    }
-    if (m.isValid()) {
-      return m;
-    }
-    return null;
+  /**
+   * Parses a date from a value.
+   * @param value The value to parse.
+   * @param parseFormat The expected format of the value being parsed
+   *     (type is implementation-dependent).
+   * @returns The parsed date, or null if date could not be parsed.
+   */
+  parse(value: any, parseFormat: any): Moment | null {
+    return moment(value, parseFormat);
   }
 
+  /**
+   * Formats a date as a string.
+   * @param date The value to parse.
+   * @param displayFormat The format to use to display the date as a string.
+   * @returns The parsed date, or null if date could not be parsed.
+   */
   format(date: Moment, displayFormat: any): string {
-    if (date) {
-      return date.format(displayFormat);
+    if (!date.isValid()) {
+      throw Error('MomentDateAdapter: Cannot format invalid date.');
     }
-    return '';
+    return date.locale('es').format(displayFormat);
   }
 
+  /**
+   * Adds the given number of years to the date. Years are counted as if flipping 12 pages on the
+   * calendar for each year and then finding the closest date in the new month. For example when
+   * adding 1 year to Feb 29, 2016, the resulting date will be Feb 28, 2017.
+   * @param date The date to add years to.
+   * @param years The number of years to add (may be negative).
+   * @returns A new date equal to the given one with the specified number of years added.
+   */
   addCalendarYears(date: Moment, years: number): Moment {
-    return date.clone().add(years, 'y');
+    return date.clone().add(years, 'years');
   }
-
+  /**
+   * Adds the given number of months to the date. Months are counted as if flipping a page on the
+   * calendar for each month and then finding the closest date in the new month. For example when
+   * adding 1 month to Jan 31, 2017, the resulting date will be Feb 28, 2017.
+   * @param date The date to add months to.
+   * @param months The number of months to add (may be negative).
+   * @returns A new date equal to the given one with the specified number of months added.
+   */
   addCalendarMonths(date: Moment, months: number): Moment {
-    return date.clone().add(months, 'M');
+    return date.clone().add(months, 'months');
   }
 
+  /**
+   * Adds the given number of days to the date. Days are counted as if moving one cell on the
+   * calendar for each day.
+   * @param date The date to add days to.
+   * @param days The number of days to add (may be negative).
+   * @returns A new date equal to the given one with the specified number of days added.
+   */
   addCalendarDays(date: Moment, days: number): Moment {
-    return date.clone().add(days, 'd');
+    return date.clone().add(days, 'days');
   }
 
-  setLocale(locale: any): void {
-    this.localeData = moment.localeData(locale);
+  /**
+   * Gets the RFC 3339 compatible date string (https://tools.ietf.org/html/rfc3339)  for the given
+   * date.
+   * @param date The date to get the ISO date string for.
+   * @returns The ISO date string date string.
+   */
+  getISODateString(date: Moment): string {
+    return date.toISOString();
   }
 
-  compareDate(first: Moment, second: Moment): number {
-    return first.diff(second, 'seconds', true);
-  }
-
-  sameDate(first: any | Moment, second: any | Moment): boolean {
-    if (first == null) {
-      return second == null;
-    } else if (isMoment(first)) {
-      return first.isSame(second);
-    }
-    return super.sameDate(first, second);
-  }
-
-  clampDate(date: Moment, min?: any | Moment, max?: any | Moment): Moment {
-    if (min && date.isBefore(min)) {
-      return min;
-    } else if (max && date.isAfter(max)) {
-      return max;
-    }
-    return date;
-  }
-
+  /**
+   * Checks whether the given date is valid.
+   * @param date The date to check.
+   * @returns Whether the date is valid.
+   */
   isValid(date: Moment): boolean {
     return date.isValid();
-  };
+  }
 
-  isDateInstance(obj: Object): boolean {
-    return moment.isMoment(obj);
-  };
-
-  toIso8601(date: Moment): string {
-    return date.format();
-  };
-
-  fromIso8601(iso8601String: string): Moment {
-    return moment(iso8601String);
-  };
-
+  /**
+   * Checks whether the given object is considered a date instance by this DateAdapter.
+   * @param obj The object to check
+   * @returns Whether the object is a date instance.
+   */
+  isDateInstance(date: Moment): boolean {
+    return moment.isMoment(date);
+  }
 }
